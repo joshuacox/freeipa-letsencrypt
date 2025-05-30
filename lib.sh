da1e1963935ea0634434736de3e5598675547b23
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-05-30 09:55:11 -0500

    URL correction

--- lib.sh
+++ lib.sh
@@ -26,7 +26,7 @@ do_roots () {
 
 do_intermediaries () {
   CERTS2=("e5.pem" "e6.pem" "e7.pem" "e8.pem" "e9.pem" "r10.pem" "r11.pem" "r12.pem" "r13.pem" "r14.pem")
-  do_certs ${CERTS2} "https://letsencrypt.org/2024"
+  do_certs ${CERTS2} "https://letsencrypt.org/certs/2024"
 }
 
 check_dirman () {
